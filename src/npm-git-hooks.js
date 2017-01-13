4f6060098db434f154caefb65f11ec0468b85244
Ripley511 <pascal.vaccaro@gmail.com>
2017-01-13 11:43:55 +0100

    Test for pre-commit hook

--- src/npm-git-hooks.js
+++ src/npm-git-hooks.js
@@ -131,6 +131,7 @@ function run(operation, fileList) {
     try {
       const pkgPath = utils.resolve(pkg.absolute, 'package.json');
       const config = getPackageConfig(require(pkgPath));
+      console.log(operation, config[operation]);
       const user = git.getUsername();
       if (config.restrictions['skip-users'].indexOf(user) >= 0) {
         console.log(`User ${user} does not need to run ${operation} tasks in ${pkg.name}, moving on...`);
